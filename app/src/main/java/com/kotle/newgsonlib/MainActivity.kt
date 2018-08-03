bd5ed4358972258d5b9d3ed795bb5e97ad9c03d5
weijiale <kotle@vip.qq.com>
2018-08-03 17:25:08 +0800

    copy源码

--- app/src/main/java/com/kotle/newgsonlib/MainActivity.kt
+++ app/src/main/java/com/kotle/newgsonlib/MainActivity.kt
@@ -2,11 +2,18 @@ package com.kotle.newgsonlib
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.kotle.gsonlib.Gson
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val s="{\"name\":\"yizisu\",\"age\":18}"
+        val gson=Gson()
+        val json = gson.fromJson<Bean>(s, Bean::class.java)
+        println(json.toString())
     }
+    data class Bean(val name:String,
+                    val age:Int)
 }
